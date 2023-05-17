fe2e621c6fec96b65611387eb4de86f6730c3e09
Tanvir <toukir.developer.bd@gmail.com>
2023-05-18 01:38:57 +0600

    Section: add header section

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,3 +1,13 @@
+import { Outlet } from "react-router-dom";
+import Footer from "../pages/Shared/Footer/Footer";
+import Header from "../pages/Shared/Header/Header";
+
 export default function Main() {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
+  );
 }
